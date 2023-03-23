4cf1a64aa3e4b1d2b50c7b51206dcd72f81789e6
piaotaijiong <127836167+piaotaijiong@users.noreply.github.com>
2023-03-23 19:06:46 +0900

    Update ppi_bot.py

--- ppi_bot.py
+++ ppi_bot.py
@@ -172,38 +172,27 @@ async def replicate(ctx, role_id: int):
     await ctx.send(f"Role '{role_to_replicate.name}' has been replicated, and the new role has been assigned to the members with the original role.")
 
 
-@bot.command()
-async def image(ctx):
-    message = ctx.message
-    Text = ""
-    learn = message.content.split(" ")
-    vrsize = len(learn)  # array size
-    vrsize = int(vrsize)
-    for i in range(1, vrsize):  # Recognize text with spaces
-        Text = Text + " " + learn[i]
-    print(Text.strip())  # command entered
+@bot.command(name='말하기')
+async def speak(ctx, message_id: int = None):
+    view = CustomView()
+    buttons = [
+        ButtonClick("스페인어", view),
+        ButtonClick("중국어", view),
+        ButtonClick("일본어", view),
+        ButtonClick("영어", view),
+        ButtonClick("프랑스어", view),
+        ButtonClick("독일어", view),
+    ]
 
-    randomNum = random.randrange(0, 40)  # random image number
+    for button in buttons:
+        view.add_button(button)
 
-    location = Text
-    enc_location = urllib.parse.quote(location)
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=' + enc_location
-    print(url)
-    req = Request(url, headers=hdr)
-    html = urllib.request.urlopen(req)
-    bsObj = bs4.BeautifulSoup(html, "html.parser")
-    imgfind1 = bsObj.find('div', {'class': 'photo_grid _box'})
-    imgfind2 = imgfind1.findAll('a', {'class': 'thumb _thumb'})
-    imgfind3 = imgfind2[randomNum]
-    imgfind4 = imgfind3.find('img')
-    imgsrc = imgfind4.get('data-source')
-    print(imgsrc)
-    embed = discord.Embed(
-        color=discord.Colour.green()
-    )
-    embed.set_image(url=imgsrc)
-    await ctx.send(embed=embed)
+    view.user_mentions = await load_user_mentions(ctx.guild)
+
+    embed = discord.Embed(title="말하기 스터디 참여 현황")
+    for button in buttons:
+        embed.add_field(name=button.label, value="No one has clicked yet!", inline=True)
+    await ctx.send(embed=embed, view=view)
     
 #Run the bot
 bot.run(TOKEN)
